6f51b358c609e66384da3a63147471af5cee8be9
Jess Lodge <jessicaklodge@gmail.com>
2018-05-19 00:48:57 +0100

    updated projects and skills

--- js/app.js
+++ js/app.js
@@ -8,11 +8,22 @@ function init() {
   //   $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 200, 'linear');
   // });
 
-  $('.toggle').click(function(e){
+  $('.toggle').click(function(){
     $('.showRole').slideToggle('slow');
   });
 
+  // Add active class to the current button (highlight it)
+  const nav = document.querySelector('.navbar');
+  const navtab = nav.querySelector('.navbar-item');
+  for (let i = 0; i < navtab.length; i++) {
+    navtab[i].addEventListener('click', function() {
+      const current = document.querySelector('.active');
+      console.log(navtab);
+      current[0].className = current[0].className.replace('active', '');
+      this.className += 'active';
 
+    });
+  }
 
 
 }

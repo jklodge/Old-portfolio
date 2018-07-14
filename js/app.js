8edbf53c7f79cbf2828ea19f69ce8f2ecb32765d
Jess Lodge <jessicaklodge@gmail.com>
2018-07-15 00:40:14 +0100

    updated projects to show one at a time

--- js/app.js
+++ js/app.js
@@ -20,6 +20,52 @@ function init() {
     $('.hidden-first').toggle();
   });
 
+  $('#one').click(function(){
+    $('.two, .three, .four, .five').hide();
+    $('.one').show();
+
+  });
+
+  $('#two').click(function(){
+    $('.one, .three, .four, .five').hide();
+    $('.two').show();
+  });
+
+  $('#three').click(function(){
+    $('.one, .two, .four, .five').hide();
+    $('.three').show();
+  });
+
+  $('#four').click(function(){
+    $('.one, .three, .two, .five').hide();
+    $('.four').show();
+  });
+
+  $('#five').click(function(){
+    $('.one, .three, .four, .two').hide();
+    $('.five').show();
+  });
+
+
+  $('.projSelector li').click(function() {
+    $('.projSelector li').removeClass('selected');
+    $(this).addClass('selected');
+  });
+  // var proj = document.getElementById('projSelector');
+  // proj.addEventListener('click',function(e){
+  //   const projClass = e.target.classList.value;
+  //   console.log(projClass);
+  //   projClass.display = 'none';
+  // },false);
+
+  // $('li > a').click(function(){
+  //   const theClass = this.className;  // "this" is the element clicked
+  //   // $(theClass).addClass('show');
+  //   $(this).addClass('show');
+  //   // divsToHide[i].style.display = "none";
+  //   console.log(theClass);
+  // });
+
 
   $('.homeTab').click(typeWriter);
 
@@ -44,4 +90,7 @@ function init() {
   }
 
 }
+
+
+
 $(init);
